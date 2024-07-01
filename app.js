5ab770c3a50c6ad2913174baef2bc8ed49977dcf
test <test@test.com>
2024-07-01 10:58:05 +0500

    new

--- app.js
+++ app.js
@@ -21,26 +21,95 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+// 'use strict';
 
-const numberOfFilms  = +prompt("Сколько фильмов уже просмотрели?" ,'');
+// const numberOfFilms  = +prompt("Сколько фильмов уже просмотрели?" ,'');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt ('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
 
-      personalMovieDB.movies[a] = b
-      personalMovieDB.movies[c] = d
+//       personalMovieDB.movies[a] = b
+//       personalMovieDB.movies[c] = d
 
-      console.log(personalMovieDB);
-      
+//       console.log(personalMovieDB);
+
+"use strict";
+
+// if (4==5) {
+//     console.log('ok');
+
+// } else {
+//     console.log('error')
+// }
+
+
+// const num = 50
+// if ( num < 49) {
+//     console.log('error')
+// } else if (num > 100) {
+//     console.log('more')
+// } else {
+//     console.log('ok')
+// }
+   
+
+
+// const num = 50
+// switch  (num) {
+//     case 49:
+//         console.log('wrong')
+//         break
+//         case 100:
+//             console.log('wrong')
+//         break;
+//         case 53:
+//             console.log('in point')
+//             break;
+// default:
+//     console.log('empty')
+//     break;
+// }
+
+// const hamb = true
+// const fries = true
+
+// if  (hamb && fries) {
+//     console.log('me')
+// }
+
+// console.log((hamb && fries))
+
+
+// if ( 10 == 1){
+//     console.log('ok')
+
+// } else {
+//     console.log('error')
+// }
+
+
+// const hamb = 2
+// const fries = 1
+// const cola = 0
+
+// console.log(hamb === 3 && cola  == 1 && fries)
+
+// if (hamb === 3 && cola == 1 && fries) {
+//     console.log('good')
+// } else {
+//     console.log ('leave')
+ 
+// }
+   
+
