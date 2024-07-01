0ecb56341eca778ae0ab796488ac37600006317d
test <test@test.com>
2024-07-01 11:02:34 +0500

    ignored

---
+++ accordion.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".at-title").click(function () {
+      $(this)
+        .toggleClass("active")
+        .next(".at-tab")
+        .slideToggle()
+        .parent()
+        .siblings()
+        .find(".at-tab")
+        .slideUp()
+        .prev()
+        .removeClass("active");
+    });
+  });
+  
